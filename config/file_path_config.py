08db218c766a424e48ce0a2e0b342bcf10e99576
arrnos <2468732648@qq.com>
2019-11-30 09:34:12 +0800

    fix path

--- config/file_path_config.py
+++ config/file_path_config.py
@@ -17,12 +17,12 @@ assert plat in ("linux", "windows", "darwin"), "platform is not linux or windows
 PROJECT_NAME = "Test"
 
 if plat == "linux":
-    user = getpass.getpass()
+    user = "zhangmeng"
     data_path = os.path.join("/home/%s/" % user, "project_data", PROJECT_NAME)
 elif plat == "windows":
     data_path = os.path.join("E:\project_data", PROJECT_NAME)
 else:
-    user = getpass.getpass()
+    user = "arrnos"
     data_path = os.path.join("/Users/", user, "project_data", PROJECT_NAME)
 
 assert os.path.isdir(data_path), "%s不存在！" % data_path
@@ -32,19 +32,20 @@ xgb_path = os.path.join(data_path, "xgb")
 
 feature_map_file = os.path.join(xgb_path, "train_feature_map")
 
-# train_libsvm_file = os.path.join(data_path, "train_libsvm_feature_sample")
-# test_libsvm_file = os.path.join(data_path, "test_libsvm_feature_sample")
-# train_raw_data_file = os.path.join(data_path, "merged_raw_train_feature_sample")
-# test_raw_data_file = os.path.join(data_path, "merged_raw_test_feature_sample")
-# train_csv_file = os.path.join(data_path, "train_csv_feature_sample")
-# test_csv_file = os.path.join(data_path, "test_csv_feature_sample")
-
-train_libsvm_file = os.path.join(data_path, "train_libsvm_feature")
-test_libsvm_file = os.path.join(data_path, "test_libsvm_feature")
-train_raw_data_file = os.path.join(data_path, "merged_raw_train_feature")
-test_raw_data_file = os.path.join(data_path, "merged_raw_test_feature")
-train_csv_file = os.path.join(data_path, "train_csv_feature")
-test_csv_file = os.path.join(data_path, "test_csv_feature")
+if plat == "linux":
+    train_libsvm_file = os.path.join(data_path, "train_libsvm_feature")
+    test_libsvm_file = os.path.join(data_path, "test_libsvm_feature")
+    train_raw_data_file = os.path.join(data_path, "merged_raw_train_feature")
+    test_raw_data_file = os.path.join(data_path, "merged_raw_test_feature")
+    train_csv_file = os.path.join(data_path, "train_csv_feature")
+    test_csv_file = os.path.join(data_path, "test_csv_feature")
+else:
+    train_libsvm_file = os.path.join(data_path, "train_libsvm_feature_sample")
+    test_libsvm_file = os.path.join(data_path, "test_libsvm_feature_sample")
+    train_raw_data_file = os.path.join(data_path, "merged_raw_train_feature_sample")
+    test_raw_data_file = os.path.join(data_path, "merged_raw_test_feature_sample")
+    train_csv_file = os.path.join(data_path, "train_csv_feature_sample")
+    test_csv_file = os.path.join(data_path, "test_csv_feature_sample")
 
 if __name__ == '__main__':
     print(os.path.exists(data_path))
