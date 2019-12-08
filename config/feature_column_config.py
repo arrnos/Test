34abaa09f929819fa79cf71c5c8a8d073df398ae
arrnos <2468732648@qq.com>
2019-12-08 23:46:29 +0800

    fix log

--- config/feature_column_config.py
+++ config/feature_column_config.py
@@ -52,9 +52,9 @@ def log_min_max_func(f):
     min_value, max_value = LOG_MIN_MAX_DICT[f]
 
     def func(x):
-        x_float = tf.cast(x, tf.float32)
+        x_float = tf.math.log1p(tf.cast(x, tf.float32))
         result = tf.where(x_float >= max_value, 1.0,
-                          1.0 * (tf.math.log1p(x_float) - min_value) / (max_value - min_value))
+                          1.0 * (x_float - min_value) / (max_value - min_value))
         return result
 
     return func
@@ -67,41 +67,43 @@ dense_process_dict = dict(
 # =============================连续特征=======================================
 
 # dense feature
-dense_features = [fc.numeric_column(feature, normalizer_fn=dense_process_dict[feature]) for feature in
-                  CONTINUOUS_FEATURE_USE_LIST]
-
+dense_features = {f_name: fc.numeric_column(f_name, normalizer_fn=dense_process_dict[f_name]) for f_name in
+                  CONTINUOUS_FEATURE_USE_LIST}
+# dense bucket
+dense_features_bucket = {f_name: fc.bucketized_column(column, dense_bound_list) for f_name, column in
+                         dense_features.items()}
 # dense embedding
-dense_features_emb = [fc.embedding_column(fc.bucketized_column(feature, dense_bound_list),
-                                          BUCKTE_EMBEDDING_SIZE) for feature in dense_features]
+dense_features_emb = {f_name: fc.embedding_column(column, BUCKTE_EMBEDDING_SIZE) for f_name, column in
+                      dense_features_bucket.items()}
 
 # ===========================离散特征===============================
 
 # sparse feature
 sparse_features = \
-    [fc.categorical_column_with_vocabulary_list(f_name, read_feature_values(f_name)) for f_name in
-     CATEGORY_FEATURE_USE_LIST]
+    {f_name: fc.categorical_column_with_vocabulary_list(f_name, read_feature_values(f_name), num_oov_buckets=10) for
+     f_name in CATEGORY_FEATURE_USE_LIST}
 
 # sparse feature one_hot
 sparse_features_one_hot = \
-    [fc.indicator_column(feature) for feature in sparse_features]
+    {f_name: fc.indicator_column(column) for f_name, column in sparse_features.items()}
 
 # sparse feature embedding
 sparse_features_emb = \
-    [fc.embedding_column(feature, SPARSE_EMBEDDING_SIZE) for feature in sparse_features]
+    {f_name: fc.embedding_column(column, SPARSE_EMBEDDING_SIZE) for f_name, column in sparse_features.items()}
 
 # ========================= LR feature column =============================
 
 LinerColumns = [
-    # sparse_features_one_hot,
-    dense_features
+    sparse_features_one_hot.values(),
+    dense_features.values()
 ]
 
 LinerColumns = list(chain(*LinerColumns))
 
 # ========================= FM feature column =============================
 InteractionColumns = [
-    sparse_features_emb,
-    # dense_features_emb
+    sparse_features_emb.values(),
+    dense_features_emb.values()
 ]
 
 InteractionColumns = list(chain(*InteractionColumns))
@@ -113,49 +115,63 @@ DNNColumns = InteractionColumns
 
 # ==========================   Test module =================================
 def get_test_tensor():
-    example = {"label": 1, "account_age": 27, "alignment_day": "22", "alignment_day_of_week": "2", "alignment_hour": 12,
-               "alignment_month": "9", "alignment_year": "2019", "all_call_num": 22,
-               "avgerage_waiting_time_of_dialogue": 12, "opp_today_following_num": 5,
-               "consult_type": "ALL", "contain_education_or_promotion_key_word_in_dialogue": "",
-               "contain_price_key_word_in_dialogue": "1", "create_user": "system", "delta_days_from_entryDate": 12,
-               "delta_time_of_create_time_and_operator_time": 100, "dialogue_start_at": "noon",
-               "duration_of_dialogue": 13, "educationalBackground": "1", "first_proj_id": "53",
-               "gender": "M", "graduateSchool": "武汉职业技术学院", "major": "市场营销",
-               "marriageState": "1", "message_opp_distribution_num": 10, "message_opp_distribution_saturation": 0.1,
-               "message_opp_limit_num": 20, "nationalEdu": "1", "number_of_dialogue": 7,
-               "number_of_student_dialogue": 3, "online_opp_distribution_num": 18,
-               "online_opp_distribution_saturation": 0.3,
-               "online_opp_limit_num": 60, "opp_completed_rate": 0.2, "opp_following_num": 7,
-               "oppor_source": "ONLINE_CS_GREATBEAR", "pastNday_advertiser_applied_ratio": 0.1,
-               "pastNday_advertiser_distribution_num": 5000, "pastNday_advertiser_order_num": 321,
-               "pastNday_applied_ratio_on_siteId": 0.05, "pastNday_distribution_num_on_siteId": 100,
-               "pastNday_order_num_on_siteId": 123, "past_n_days_acc_alignment_num": 123,
-               "past_n_days_acc_order_amount": 100000, "past_n_days_acc_order_avg_amount": 10000,
-               "past_n_days_acc_order_num": 5, "past_n_days_acc_order_rate": 0.04,
-               "posLevel": "1", "promotion_type": "SEM", "quantum_id": "2030006",
-               "race": "汉", "recycle_opp_distribution_num": 10, "recycle_opp_distribution_saturation": 0.05,
-               "recycle_opp_limit_num": 12, "residenceType": "1", "site_source": "微信朋友圈移动平台",
-               "student_cellphone_midnum": "",  # "student_dialogue_fenci":"好",
-               "timedelta_from": 2, "today_unfollowed_num": 12, "twice_consult": "", "valid_call_num": 12}
+    example = {"label": [1], "account_age": [27, 88], "alignment_day": ["22"], "alignment_day_of_week": ["2"],
+               "alignment_hour": [12],
+               "alignment_month": ["9"], "alignment_year": ["2019", "2018"], "all_call_num": [22],
+               "avgerage_waiting_time_of_dialogue": [12], "opp_today_following_num": [5],
+               "consult_type": ["ALL"], "contain_education_or_promotion_key_word_in_dialogue": [""],
+               "contain_price_key_word_in_dialogue": ["1"], "create_user": ["system"],
+               "delta_days_from_entryDate": [120, 10000],
+               "delta_time_of_create_time_and_operator_time": [100], "dialogue_start_at": ["noon"],
+               "duration_of_dialogue": [13], "educationalBackground": ["1"], "first_proj_id": ["53"],
+               "gender": ["M"], "graduateSchool": ["武汉职业技术学院"], "major": ["市场营销"],
+               "marriageState": ["1"], "message_opp_distribution_num": [10],
+               "message_opp_distribution_saturation": [0.1],
+               "message_opp_limit_num": [20], "nationalEdu": ["1"], "number_of_dialogue": [7],
+               "number_of_student_dialogue": [3], "online_opp_distribution_num": [18],
+               "online_opp_distribution_saturation": [0.3],
+               "online_opp_limit_num": [60], "opp_completed_rate": [0.2], "opp_following_num": [7],
+               "oppor_source": ["ONLINE_CS_GREATBEAR"], "pastNday_advertiser_applied_ratio": [0.1],
+               "pastNday_advertiser_distribution_num": [5000], "pastNday_advertiser_order_num": [321],
+               "pastNday_applied_ratio_on_siteId": [0.05], "pastNday_distribution_num_on_siteId": [100],
+               "pastNday_order_num_on_siteId": [123], "past_n_days_acc_alignment_num": [123],
+               "past_n_days_acc_order_amount": [100000], "past_n_days_acc_order_avg_amount": [10000],
+               "past_n_days_acc_order_num": [5], "past_n_days_acc_order_rate": [0.04],
+               "posLevel": ["1"], "promotion_type": ["SEM"], "quantum_id": ["2030006"],
+               "race": ["汉"], "recycle_opp_distribution_num": [10], "recycle_opp_distribution_saturation": [0.05],
+               "recycle_opp_limit_num": [12], "residenceType": ["1"], "site_source": ["微信朋友圈移动平台"],
+               "student_cellphone_midnum": [""],  # "student_dialogue_fenci":["好"],
+               "timedelta_from": [2], "today_unfollowed_num": [12], "twice_consult": [""], "valid_call_num": [12]}
 
     example_tensor = {x: tf.constant(y) for x, y in example.items()}
     return example_tensor
 
 
-def test_reshaped_emb(example_tensor):
-    # flatten shape(batch_size, column_num * embedding_size)
-    # features = tf.io.parse_example(..., features=make_parse_example_spec(columns))
-    feature_layer = tf.keras.layers.DenseFeatures(InteractionColumns)
-    flatten_emb = feature_layer(example_tensor)
-    interaction_column_num = len(InteractionColumns)
+def valid_columns():
+    example_tensor = get_test_tensor()
 
-    assert SPARSE_EMBEDDING_SIZE == BUCKTE_EMBEDDING_SIZE
+    test_dense_feature_columns = [
+        (f_name, (dense_features[f_name], dense_features_bucket[f_name], dense_features_emb[f_name]))
+        for f_name in test_dense_features]
+    for f_name, (sparse, one_hot, emb) in test_dense_feature_columns:
+        print(f_name, example_tensor[f_name].numpy())
+        print(MIN_MAX_DICT[f_name])
+        print(LOG_MIN_MAX_DICT[f_name])
 
-    reshaped_emb = tf.reshape(flatten_emb, (-1, interaction_column_num, SPARSE_EMBEDDING_SIZE), "reshape_embedding")
+        print(tf.keras.layers.DenseFeatures([sparse])(example_tensor))
+        print(tf.keras.layers.DenseFeatures([one_hot])(example_tensor))
+        print(tf.keras.layers.DenseFeatures([emb])(example_tensor))
+        print()
 
+    test_sparse_feature_columns = [
+        (f_name, (sparse_features[f_name], sparse_features_one_hot[f_name], sparse_features_emb[f_name]))
+        for f_name in test_sparse_features]
+    for f_name, (sparse, one_hot, emb) in test_sparse_feature_columns:
+        print(f_name, example_tensor[f_name].numpy())
 
-def test_dense(example_tensor):
-    pass
+        print(tf.keras.layers.DenseFeatures([one_hot])(example_tensor))
+        print(tf.keras.layers.DenseFeatures([emb])(example_tensor))
+        print()
 
 
 if __name__ == '__main__':
@@ -164,8 +180,8 @@ if __name__ == '__main__':
 
     print("DNN和交互模块:", len(InteractionColumns))
     print(InteractionColumns)
+    print()
 
-    example_tensor = get_test_tensor()
-    test_reshaped_emb(example_tensor)
-
-    test_dense(example_tensor)
+    test_dense_features = ["delta_days_from_entryDate", "account_age", "timedelta_from"]
+    test_sparse_features = ["posLevel", "alignment_day", "alignment_year"]
+    valid_columns()
